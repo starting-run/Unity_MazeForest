8e0c92d0793c980a8fc735d655de81040f125c16
usfree <tlrmsjtm77@naver.com>
2024-06-06 01:58:41 +0900

    OX퀴즈 이미지 추가

--- Assets/Script/ScenarioEngine.cs
+++ Assets/Script/ScenarioEngine.cs
@@ -15,25 +15,28 @@ public class ScenarioEngine : MonoBehaviour
     public GameObject[] cameras;
 
     public string[] objectsName;
-    
-        
-    
+
+
+
     //private Image back;
     private Image front;
-    private RawImage video; 
+    private RawImage video;
     private TextMeshProUGUI text;
     private Canvas dialog;
     private VideoPlayer player;
 
-    private Dictionary<string, GameObject> map = new Dictionary<string,GameObject>();
+    private Dictionary<string, GameObject> map = new Dictionary<string, GameObject>();
+
+    private CanvasGroup canvasGroup;
 
     void Start()
-    {        
+    {
+        canvasGroup = canvas.GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            canvasGroup = canvas.gameObject.AddComponent<CanvasGroup>();
+        }
 
-        //front = canvas.transform.Find("Front").GetComponent<Image>();
-        //video = canvas.transform.Find("Video").GetComponent<RawImage>();
-        //text = canvas.transform.Find("Text").GetComponent<TextMeshProUGUI>();
-        //player = transform.GetComponent<VideoPlayer>();
         dialog = canvas.transform.Find("Dialog").GetComponent<Canvas>();
         if (objectsName.Length > 0)
         {
@@ -47,12 +50,15 @@ public class ScenarioEngine : MonoBehaviour
 
     }
 
-    public IEnumerator PlayScript(string script, string question = "") {         
-        canvas.enabled = true;
-        foreach(string token in script.Split('\n')) {
+    public IEnumerator PlayScript(string script, string question = "")
+    {
+        yield return StartCoroutine(FadeCanvas(true, 0f)); // Fade In
+        foreach (string token in script.Split('\n'))
+        {
             string[] tokens = Parsing(token);
             string fun = tokens[0];
-            if ( fun  == "video" ) {
+            if (fun == "video")
+            {
                 string clip = tokens[1];
                 front.enabled = false;
                 dialog.enabled = false;
@@ -60,21 +66,30 @@ public class ScenarioEngine : MonoBehaviour
                 player.clip = Resources.Load<VideoClip>(clip);
                 float len = (float)player.clip.length;
                 player.Play();
-                yield return new WaitForSeconds( len );
-            }else if ( fun == "text" ) {
-                text.enabled = true;                                            
+                yield return new WaitForSeconds(len);
+            }
+            else if (fun == "text")
+            {
+                text.enabled = true;
                 text.text = tokens[1];
-                yield return new WaitForSeconds( float.Parse(tokens[2] == "" ? "0"  : tokens[2]) );                
-            } else if ( fun == "image" ) {
+                yield return new WaitForSeconds(float.Parse(tokens[2] == "" ? "0" : tokens[2]));
+            }
+            else if (fun == "image")
+            {
                 front.enabled = true;
-                video.enabled = false;                
+                video.enabled = false;
                 dialog.enabled = false;
                 front.sprite = Resources.Load<Sprite>(tokens[1]);
-                yield return new WaitForSeconds( float.Parse(tokens[2] == "" ? "0"  : tokens[2]) );                
-            } else if  ( fun == "wait") {
-                yield return new WaitForSeconds( float.Parse(tokens[1] == "" ? "2"  : tokens[1]) );                
-            } else if  ( fun == "dialog") {                
+                yield return new WaitForSeconds(float.Parse(tokens[2] == "" ? "0" : tokens[2]));
+            }
+            else if (fun == "wait")
+            {
+                yield return new WaitForSeconds(float.Parse(tokens[1] == "" ? "2" : tokens[1]));
+            }
+            else if (fun == "dialog")
+            {
                 dialog.enabled = true;
+                dialog.transform.Find("Question_Image").GetComponent<Image>().enabled = false;
                 //front.enabled = false;
                 //video.enabled = false;
                 string ch = tokens[1];
@@ -83,13 +98,14 @@ public class ScenarioEngine : MonoBehaviour
                 {
                     dialog.transform.Find("Image").GetComponent<Image>().enabled = true;
                     dialog.transform.Find("Image").GetComponent<Image>().sprite = Resources.Load<Sprite>(ch);
+                    dialog.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = text;
                 }
                 else
                 {
                     dialog.transform.Find("Image").GetComponent<Image>().enabled = false;
+                    dialog.transform.Find("Text_Center").GetComponent<TextMeshProUGUI>().text = text;
                 }
-                dialog.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = text;
-                yield return new WaitForSeconds( float.Parse(tokens[3] == "" ? "0"  : tokens[3]) );
+                yield return new WaitForSeconds(float.Parse(tokens[3] == "" ? "0" : tokens[3]));
 
             }
             else if (fun == "question")
@@ -99,48 +115,69 @@ public class ScenarioEngine : MonoBehaviour
                 //video.enabled = false;
                 string ch = tokens[1];
                 string text = question;
-                if (ch != "null")
-                {
-                    dialog.transform.Find("Image").GetComponent<Image>().enabled = true;
-                    dialog.transform.Find("Image").GetComponent<Image>().sprite = Resources.Load<Sprite>(ch);
-                }
-                else
-                {
-                    dialog.transform.Find("Image").GetComponent<Image>().enabled = false;
-                }
-                dialog.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = text;
+
+                dialog.transform.Find("Question_Image").GetComponent<Image>().enabled = true;
+                dialog.transform.Find("Question_Image").GetComponent<Image>().sprite = Resources.Load<Sprite>(ch);
+                dialog.transform.Find("Text_Center").GetComponent<TextMeshProUGUI>().text = text;
+
                 yield return new WaitForSeconds(float.Parse(tokens[3] == "" ? "0" : tokens[3]));
-            } else if ( fun == "camera") {
-                for(int i = 0; i < cameras.Length; i++) cameras[i].SetActive(false);
-                cameras[int.Parse(tokens[1])].SetActive(true);                        
-            } else if ( fun == "show") {
-                canvas.enabled = tokens[1] == "false" ? false : true;
-            } else if ( fun == "moveto") {                
-                GameObject obj= map[tokens[1]];
+            }
+            else if (fun == "camera")
+            {
+                for (int i = 0; i < cameras.Length; i++) cameras[i].SetActive(false);
+                cameras[int.Parse(tokens[1])].SetActive(true);
+            }
+            else if (fun == "show")
+            {
+                bool show = tokens[1] == "true";
+                yield return StartCoroutine(FadeCanvas(show, 0.5f)); // Fade In or Fade Out
+            }
+            else if (fun == "moveto")
+            {
+                GameObject obj = map[tokens[1]];
                 Vector3 target = map[tokens[2]].transform.position;
-                float duration = float.Parse(tokens[3] == "" ? "3"  : tokens[3]);
-                
+                float duration = float.Parse(tokens[3] == "" ? "3" : tokens[3]);
+
                 float elapsedTime = 0f;
                 Vector3 startingPos = obj.transform.position;
-                while (elapsedTime < duration) {
+                while (elapsedTime < duration)
+                {
                     obj.transform.position = Vector3.Lerp(startingPos, target, elapsedTime / duration);
                     obj.transform.forward = (target - obj.transform.position).normalized;
                     elapsedTime += Time.deltaTime;
                     yield return null;
-                }        
+                }
                 obj.transform.position = target;
             }
-        }        
-        canvas.enabled = false;
+        }
+        yield return StartCoroutine(FadeCanvas(false, 0.5f)); // Fade Out
     }
 
-    string[] Parsing(string cmd) {
+    private IEnumerator FadeCanvas(bool fadeIn, float duration)
+    {
+        float startAlpha = fadeIn ? 0f : 1f;
+        float endAlpha = fadeIn ? 1f : 0f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / duration);
+            canvasGroup.alpha = alpha;
+            yield return null;
+        }
+
+        canvasGroup.alpha = endAlpha;
+    }
+
+    string[] Parsing(string cmd)
+    {
         cmd = cmd.Replace("(", ",").Replace(")", "");
-        string[] tokens  = cmd.Split(',');               
-        for(int i = 0; i < tokens.Length; i++) {
+        string[] tokens = cmd.Split(',');
+        for (int i = 0; i < tokens.Length; i++)
+        {
             tokens[i] = tokens[i].Trim();
         }
         return tokens;
     }
 }
-
